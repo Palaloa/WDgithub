5a9e6a3e815a76f58eaadcfcb59b56166161e0b8
Day-Dreamer\JespervandenBerg <jespermaik@gmail.com>
2017-12-05 19:04:50 +0100

    update_01 html made bare - No CSS/JS js added functionalities+

--- AJH.js
+++ AJH.js
@@ -1,8 +1,18 @@
 function showForm() {
-    document.getElementById("habit").style.display='block';
+    document.getElementById("habitForm").style.display='block';
     document.getElementById("addHabitButton").style.display='none';
  }
  
- function updateName() {
-     //FIXME
+function confirmDeleteHabit(n) {
+    if (confirm("Are you sure?") == true) {
+        deleteHabit(n);
+    } else alert("deletion cancelled");
+}
+
+function deleteHabit(n) {
+     document.getElementById(n).style.display = 'none';
+ }
+
+ function formAddHabit(){
+     document.getElementById("testPar").innerHTML = document.getElementById("habitName").value;
  }
