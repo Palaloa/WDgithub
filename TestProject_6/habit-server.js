9d17465854d905046dca48e7090df9d96a2c002f
jesperberg\JespervandenBerg <j.m.vandenberg@student.tudelft.nl>
2017-12-13 10:29:08 +0100

    Added Delete functionality, now /deletehabit&message=something will delete something from the local data.

--- TestProject_6/habit-server.js
+++ TestProject_6/habit-server.js
@@ -8,8 +8,8 @@ app.use(express.static(__dirname + "/client"));
 http.createServer(app).listen(port);
 
 var habits = [];
-var t1 = { message : "Walk 10.000 steps", type  : 1, interval : "every day"};
-var t2 = { message : "Go to the gym", type : 2, interval : "3 days a week"};
+var t1 = { message : "Walk", type  : 1, interval : "everyday"};
+var t2 = { message : "Gym", type : 2, interval : "3week"};
 habits.push(t1);
 habits.push(t2);
 
@@ -32,6 +32,33 @@ app.get("/addhabit", function (req, res) {
 		habits.push(tx);
 		console.log("Added " + tx.message);
 		res.end("Habit added successfully");
+	}
+	else {
+		res.end("Error: missing message parameter");
+	}
+});
+
+//delete a habit from the server
+app.get("/deletehabit", function (req, res) {
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	
+	if(query["message"]!==undefined) {
+		var tx = { message : query["message"]};
+
+		function findElementByMessage(element){
+			return element.message == tx.message;
+		}
+
+		var index = habits.findIndex(findElementByMessage);
+		if(index > -1){
+			habits.splice(index, 1);
+			console.log("Deleted " + tx.message);
+			res.end("Habit deleted successfully");
+		} else{
+			res.end("Error: habit not found");
+		}
+		
 	}
 	else {
 		res.end("Error: missing message parameter");
